d9f4ae314126c503b34f330d00a4d6c0b2479460
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-01-06 20:43:28 -0300

    renaming variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,32 @@
-const dataDoEvento = new Date("Dec 23, 2024 00:00:00");
-const timeStampDoEvento = dataDoEvento.getTime();
+const dataDoAniversario = new Date("Dec 23, 2024 00:00:00");
+const timeStampDoAniversario = dataDoAniversario.getTime();
 
 const contaAsHoras = setInterval(function () {
   const agora = new Date();
   const timeStampAtual = agora.getTime();
 
-  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+  const distanciaAteOAniversario = timeStampDoAniversario - timeStampAtual;
 
   const diaEmMs = 1000 * 60 * 60 * 24;
   const horaEmMs = 1000 * 60 * 60;
   const minutoEmMs = 1000 * 60;
 
-  const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-  const horasAteOEvento = Math.floor(
-    (distanciaAteOEvento % diaEmMs) / horaEmMs
+  const diasAteOAniversario = Math.floor(distanciaAteOAniversario / diaEmMs);
+  const horasAteOAniversario = Math.floor(
+    (distanciaAteOAniversario % diaEmMs) / horaEmMs
   );
-  const minutosAteOEvento = Math.floor(
-    (distanciaAteOEvento % horaEmMs) / minutoEmMs
+  const minutosAteOAniversario = Math.floor(
+    (distanciaAteOAniversario % horaEmMs) / minutoEmMs
   );
-  const segundosAteOEvento = Math.floor(
-    (distanciaAteOEvento % minutoEmMs) / 1000
+  const segundosAteOAniversario = Math.floor(
+    (distanciaAteOAniversario % minutoEmMs) / 1000
   );
 
   document.getElementById(
     "contador"
-  ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+  ).innerHTML = `${diasAteOAniversario}d ${horasAteOAniversario}h ${minutosAteOAniversario}m ${segundosAteOAniversario}s`;
 
-  if (distanciaAteOEvento < 0) {
+  if (distanciaAteOAniversario < 0) {
     clearInterval(contaAsHoras);
     document.getElementById("contador").innerHTML = "Evento expirado";
   }
